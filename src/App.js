c7d293ad3611756610d4bc6cbf02f773c0b49b37
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-23 11:28:38 +0500

    Main App file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Animation from './components/Animation';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Animation />
+    </div>
+  );
+}
+
+export default App;
